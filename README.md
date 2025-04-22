4482da565137f8818323429b0d001e48f1670b27
Alexandre Barros <alexandrebarros.dev@gmail.com>
2025-04-22 12:13:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# reactnative-sequenciador-de-senhas
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from "react-native";
+
+import styles from "./styles";
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Home works!!</Text>
+            <StatusBar style="auto" />
+        </View>
+        
+    )
+}
